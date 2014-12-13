cb1dd70da970ddd7ae57aad03ea53360200d6692
magicxie <eggcanfly@gmail.com>
2014-12-14 00:00:04 +0800

    jce types and reader

--- src/jcedecoder.js
+++ src/jcedecoder.js
@@ -1,5 +1,60 @@
 /**
  * Created by magic on 2014/12/13.
  */
+"use strict";
+var jceTyps = (function(){
+    return {
+        0 : 'Char',
+        1 : 'Short',
+        2 : 'Int32',
+        3 : 'Int64',
+        4 : 'Float',
+        5 : 'Double',
+        6 : 'String1',
+        7 : 'String4',
+        8 : 'Map',
+        9 : 'List',
+        10 : 'StructBegin',
+        11 : 'StructEnd',
+        12 : 'ZeroTag',
+        13 : 'SimpleList',
+        14 : '/',
+        15 : '/'
+    }
+})();
 
-                                 1
+var jceReader = (function() {
+
+    var _reader = function(input){
+
+        if(input == null){
+            throw new Error('input can not be null!');
+        }
+
+        this.input = input.toString(16);
+        this.indicator = -1;
+
+    };
+
+    _reader.prototype.read = function(){
+       //TODO read n length arguments
+       if(this.indicator == this.input.length){
+           return -1;
+       }else {
+           return this.input.charAt(this.indicator++);
+       }
+    }
+
+    return _reader;
+
+})();
+
+var jceDecoder = (function(reader){
+
+
+
+    return function(input){
+       return reader(input).read();
+    }
+
+})(jceReader);
